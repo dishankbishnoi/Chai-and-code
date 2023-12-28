c7a0ba829aff252de50eca482e273b68f71cca6f
Dishank Bishnoi <90502552+dishankbishnoi@users.noreply.github.com>
2023-12-28 09:00:25 +0000

    lecture 1 and lecture 2

---
+++ javascript/lecture1.js
@@ -0,0 +1,14 @@
+const systemId = 11223
+let name = "Dishank"
+var city =  "Chandigarh"
+ 
+// var is not used nowadays because it cannot deal good with scope variable & functional scope and  leads to error in several cases
+// Const values cannot be changed
+
+// Changing values of the let and var
+
+name = "Dishank Bishnoi"
+city = "Mumbai"
+
+console.log(name)
+console.table([systemId, name , city])
---
+++ javascript/lecture2_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; 
+// This enables the javascript file and code  all working in the newer versions but default javascipt works in use strict
+
+// alert() We are using node js not browser
+

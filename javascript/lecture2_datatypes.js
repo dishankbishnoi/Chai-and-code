dcfe2eac289c6ae30ba14f4a8ea2395efa2ec050
Dishank Bishnoi <90502552+dishankbishnoi@users.noreply.github.com>
2023-12-28 09:28:22 +0000

    changes

--- javascript/lecture2_datatypes.js
+++ javascript/lecture2_datatypes.js
@@ -3,3 +3,24 @@
 
 // alert() We are using node js not browser
 
+let name = "dishank " 
+
+let isLoggedIn = true
+
+
+
+// Datatypes in javascript
+
+// Boolean data type
+// String datatype
+// Bigint
+// Number
+// Null ( Standalone value)
+// Undefined = Value not defined
+// Symbol
+
+// Object 
+
+console.log(typeof 19)
+console.log(typeof null)
+console.log(typeof undefined)
